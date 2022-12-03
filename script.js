d95f4f3365a4958a3abbdb11acdb3abccf684c5b
gokdefterim <mustafaakalfa34@gmail.com>
2022-12-04 02:02:33 +0300

    destination'a moon ve mars eklendi

--- script.js
+++ script.js
@@ -17,9 +17,23 @@ function setStyle () {
 
 }
 
+
+
 function setHtml () {
-
-
+    $('.mars').click(function(){
+        $('.moon').css('border-bottom', '0px');
+        $('.moon-text').css('margin-bottom', '0px');
+        $('.europa').css('border-bottom', '0px');
+        $('.europa-text').css('margin-bottom', '0px');
+        $('.titan').css('border-bottom', '0px');
+        $('.titan-text').css('margin-bottom', '0px');
+        $('.mars').css('border-bottom', 'solid 2px #FFFFFF');
+        $('.mars-text').css('margin-bottom', '-2px');
+        $('.destination-title').html('<h1>MARS</h1>');
+        $('.destination-info').html('<p>Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!</p>');
+        $('.distance').html('<h6>AVG. DISTANCE</h6><p>225 MIL. KM</p>');
+        $('.time').html('<h6>EST. TRAVEL TIME</h6><p>9 DAYS</p>');
+      });
 }
 
 (function initialize(){
